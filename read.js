a6c2f763ba5862583c08fe4329b893d08d53d8ee
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-02-15 01:02:30 +0000

    created a program that takes text by line

---
+++ read.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+
+fs.readFile("namecolor.txt", 'utf8', function(err, data) {  
+   if (err) throw err;
+   console.log("The data on line 2 is:", data.split(/\n/)[1]);
+   
+   console.log("The data on line 1 is:", data.split(/\n/)[0]);
+});
+
