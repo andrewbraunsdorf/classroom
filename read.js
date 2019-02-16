c6abe90c56e62d0235c095262fec3cf051101f03
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-02-16 15:37:47 +0000

    committing notes

--- read.js
+++ read.js
@@ -2,8 +2,8 @@ const fs = require("fs");
 
 fs.readFile("namecolor.txt", 'utf8', function(err, data) {  
    if (err) throw err;
-   console.log("The data on line 2 is:", data.split(/\n/)[1]);
+   console.log("The data on line 2 is:", data.split('\n')[1]);
    
-   console.log("The data on line 1 is:", data.split(/\n/)[0]);
+   console.log("The data on line 1 is:", data.split('\n')[0]);
 });
 
