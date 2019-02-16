f6b6ab3714a51bcc0010fbbeb5cc93637b625ab3
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-02-16 18:54:07 +0000

    added more instructions in text file

--- project/salary.js
+++ project/salary.js
@@ -1,15 +1,23 @@
 const fs = require("fs");
 
 fs.readFile("departments.txt", 'utf8', function(err, data) {  
-	 if (err) throw err;
-   var arraydat = data.split(',')
-   
-   //for (var i = 0; i < arraydat.length; i++) {
-   //	data.split(",");
-   //	// var str = arraydat[i]
-   //	// str = str[i].slice(1, 3) + str[i].slice(4);
-   //	// 	// console.log("The data on line", i+1, "is:", data.split('\n')[i]);
-   //}
-   console.log(arraydat)
-   
+   if (err) throw err;
+    var textByLine = data.split("\n")
+    console.log(textByLine[0]);
+    console.log(textByLine);
 });
+
+
+// fs.readFile("departments.txt", 'utf8', function(err, data) {  
+// 	 if (err) throw err;
+//    var arraydat = data.split(',')
+   
+//    //for (var i = 0; i < arraydat.length; i++) {
+//    //	data.split(",");
+//    //	// var str = arraydat[i]
+//    //	// str = str[i].slice(1, 3) + str[i].slice(4);
+//    //	// 	// console.log("The data on line", i+1, "is:", data.split('\n')[i]);
+//    //}
+//    console.log(arraydat)
+   
+// });
